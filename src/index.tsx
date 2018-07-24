c7a8bfd99d86b84a17751eafbaa64e243d4ecc95
C.K. Wagaba <ckwagaba@gmail.com>
2018-07-24 11:20:47 +0300

    install HTML plugin

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReaactDOM from 'react-dom';
+
+import App from './common/App.component';
+
+ReaactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const loadHTML = new HTMLWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
 
 const config = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
   },
   module: {
     rules: [
@@ -16,7 +25,7 @@ const config = {
     ]
   },
   plugins: [
-
+    loadHTML
   ]
 };
 
